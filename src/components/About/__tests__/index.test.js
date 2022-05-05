6fd668d7e647d3b59ecf9d86d09dc9d72d5f2ff9
TTKHScriptPoetry <kimdotnetpossible@gmail.com>
2022-05-04 19:26:29 -0700

    jest framework to test About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+// import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; //offers access to custom matchers that are used to test DOM elements
+
+import About from '..';
+
+// call the cleanup function using the afterEach global function from Jest:
+afterEach(cleanup);
+
+afterEach(cleanup);
+
+// use the describe function to declare the component we're testing
+describe('About component', () => {
+    //renders About test
+    // First Test
+    it('renders', () => {
+        render(<About />);
+      });
+
+  // Second Test
+  it('matches snapshot DOM node structure', () => {
+    // use the asFragment function, which returns a snapshot of the About component
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; //rendering the App component at the root element in the HTML
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
