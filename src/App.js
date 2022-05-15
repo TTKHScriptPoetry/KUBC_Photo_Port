bbd0d1214553ea75f518dd453733674a1a0003ed
= <=>
2022-05-14 23:25:54 -0500

    implemented useState and onChange per input text change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav/';
 import About from './components/About';
 import Gallery from './components/Gallery/';
+import ContactForm from './components/Contact';
 
 // Apply useState on an array
 function App() {
@@ -26,6 +27,7 @@ function App() {
       ></Nav>
       <main>
         <div>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
